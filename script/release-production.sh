6548c129d2db6e71720f34f0f134f69e6af7289c
Simone Testa <sim.testa97@gmail.com>
2023-10-06 13:07:32 +0200

    ci: update deploy scripts

--- script/release-production.sh
+++ script/release-production.sh
@@ -1,13 +1,19 @@
 #! /bin/bash
 
-git switch main &&
+git switch main
+
+if [ $? != 0 ]; then
+  echo -e "\033[31merror\033[0m missing main branch"
+  exit 1
+fi
+
 production_last_local_commit_sha=$(git log -1 --pretty=%H | cat) &&
-git pull &&
+(git pull || git push --set-upstream origin main ) &&
 production_last_origin_commit_sha=$(git log -1 --pretty=%H | cat)
 
 if [ "$production_last_local_commit_sha" != "$production_last_origin_commit_sha" ]; then 
-  echo  -e "\033[33mwarning\033[0m your local main commit is different from remote one"
-  echo "Check the changes!"
+  echo -e "\033[33mwarning\033[0m your local main commit is different from remote one"
+  echo -e "\033[33mwarning\033[0m check the changes!"
   exit 1
 fi
 
@@ -18,4 +24,4 @@ if [[ "$production_last_commit" == "chore(release)"* ]]; then
 fi
 
 yarn release &&
-git push --follow-tags
+git push --follow-tags --set-upstream origin main
